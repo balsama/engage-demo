0cd40bad0767a961a092a2d2baff01fc4314a0d1
Adam Balsam <adam.balsam@acquia.com>
2018-09-26 10:55:30 -0400

    Set workflow states

--- scripts/configure_cloud.sh
+++ scripts/configure_cloud.sh
@@ -4,3 +4,6 @@ chmod -R +w docroot/sites/default
 cp docroot/sites/default/default.settings.php docroot/sites/default/settings.php
 echo "if (file_exists('/var/www/site-php')) { require '/var/www/site-php/engagedemo/engagedemo-settings.inc';}" >> docroot/sites/default/settings.php
 echo "\$config_directories['sync'] = '../config/sync';" >> docroot/sites/default/settings.php
+
+./scripts/set_content_directory.sh
+
---
+++ scripts/set_content_directory.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "\$config['content_directory'] = 'modules/custom/engage_demo_content/content';" >> docroot/sites/default/settings.php
