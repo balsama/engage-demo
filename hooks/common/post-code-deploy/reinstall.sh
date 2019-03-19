141dac089fcc8605310f6553b5d3234d64f00277
Adam Balsam <adam.balsam@acquia.com>
2019-03-19 10:45:22 -0400

    remove debug output from si

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -4,7 +4,7 @@ site="$1"
 target_env="$2"
 
 # Fresh install of the Lightning Demo.
-/usr/local/bin/drush9 @$site.$target_env site-install config_installer --yes -vvv
+/usr/local/bin/drush9 @$site.$target_env site-install config_installer --yes
 /usr/local/bin/drush9 @$site.$target_env pm:uninstall demo_umami_content
 /usr/local/bin/drush9 @$site.$target_env pm:enable demo_umami_content
 /usr/local/bin/drush9 @$site.$target_env user:password admin admin
