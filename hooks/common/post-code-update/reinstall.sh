3cdc13d92f3d5bf03dcde4949efa2f4b7faa1eed
Adam Balsam <balsama@users.noreply.github.com>
2019-03-19 15:18:00 -0400

    Update to core 8.7 and relevant Lightning components (#24)

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -8,4 +8,3 @@ target_env="$2"
 /usr/local/bin/drush9 @$site.$target_env pm:uninstall demo_umami_content
 /usr/local/bin/drush9 @$site.$target_env pm:enable demo_umami_content
 /usr/local/bin/drush9 @$site.$target_env user:password admin admin
-/usr/local/bin/drush9 @$site.$target_env default-content-moderation-import
