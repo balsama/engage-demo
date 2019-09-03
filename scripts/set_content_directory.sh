85a1d91d8147dc11fda03f3907ce8cdae573f601
Adam Balsam <adam.balsam@acquia.com>
2019-09-02 22:09:36 -0400

    Commit settings

--- scripts/set_content_directory.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chmod +w docroot/sites/default/settings.php
-echo "\$config['content_directory'] = 'modules/custom/engage_demo_content/content';" >> docroot/sites/default/settings.php
