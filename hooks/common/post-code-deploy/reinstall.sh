3e39873f3ac1952ac1c2126b896a756af7b05d8f
Adam Balsam <adam.balsam@acquia.com>
2019-09-03 21:41:16 -0400

    Switch to Standard profile, provide default content, and disable most resources via JSON:API Extras

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -5,5 +5,5 @@ target_env="$2"
 
 # Fresh install of the Lightning Demo.
 /usr/local/bin/drush9 @$site.$target_env site-install --existing-config --yes
-/usr/local/bin/drush9 @$site.$target_env pm:uninstall demo_umami_content
-/usr/local/bin/drush9 @$site.$target_env pm:enable demo_umami_content
+/usr/local/bin/drush9 @$site.$target_env pm:uninstall jsonapi_demo_content
+/usr/local/bin/drush9 @$site.$target_env pm:enable jsonapi_demo_content
