495d685844aef8235e4548c401b6c0927df47a1a
Adam Balsam <adam.balsam@acquia.com>
2019-10-11 07:42:10 -0400

    Update to 8.8 and latest jsonapi modules

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -4,6 +4,6 @@ site="$1"
 target_env="$2"
 
 # Fresh install of the Lightning Demo.
-/usr/local/bin/drush9 @$site.$target_env site-install --existing-config --yes
+/usr/local/bin/drush9 @$site.$target_env site-install --existing-config --account-pass=admin --yes
 /usr/local/bin/drush9 @$site.$target_env pm:uninstall jsonapi_demo_content
 /usr/local/bin/drush9 @$site.$target_env pm:enable jsonapi_demo_content
